4f36d23d8fee755e917ebd0e940384ab03356b34
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-03-22 20:57:55 -0500

    cambio

--- binarySearch.js
+++ binarySearch.js
@@ -2,30 +2,24 @@
 function binarySearch(list, value) {
     let middle = (parseInt(list.length/2));
     let listnew;
-
-        if(middle<=value){
+        if(value == middle){
+            return middle
+        }else{
+            binarySearch(listnew,value)
+        }
+        if(middle<value){
             console.log('1')
             listnew = list.slice(middle, list.length)
             console.log(listnew)
-            if(value == middle){
-                return 'valor encontrado'
-            }else{
-                binarySearch(listnew,value)
-            }
+
         }
         if(value < middle){
             console.log('2')
             listnew = list.slice(0, middle)
-            if(value == middle){
-                return 'valor encontrado'
-            }else{
-                binarySearch(listnew,value)
-            }
-        }
 
-    return 'no se encotro el numero';
+        }
 }
 
 
-console.log(binarySearch([1,2,3,4,5,6,7,8,9],8));
+console.log(binarySearch([1,2,3,4,5,6,7,8,9],3));
 
